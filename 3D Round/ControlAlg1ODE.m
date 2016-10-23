d870f4d9b5bae4f3de085b2e92269f29457e9900
cameronfr <catechboy@hotmail.com>
2016-10-23 18:20:36 +0900

    added simulated max accel/decel

--- 3D Round/ControlAlg1ODE.m
+++ 3D Round/ControlAlg1ODE.m
@@ -1,7 +1,9 @@
-[t,x] = ode45(@f2,[0,3],[10,-10]);
+[t,x] = ode45(@f2,[0,10],[10,-10]);
 
 acc = diff(x(:,2))./diff(t);
-x(2:end,3)=acc;
+x(2:end,3)=min(10,max(-10,acc));
+
+
 plot(t,x);
 xlabel('Time (s)');
 legend('position','velocity','acceleration');
@@ -9,7 +11,17 @@ legend('position','velocity','acceleration');
 
 function dxdt = f2(t,x)
 dxdt(1,1) = x(2);
-dxdt(2,1) = -sign(x(2))*(1+randn*0.15)*((x(2)^2)/(2*(abs(x(1)))));
+if(sign(x(1)) == sign(x(2)))
+    dxdt(2,1) = -3*sign(x(1));
+else
+    accel = -1.2*sign(x(2))*(1+randn*0.15)*((x(2)^2)/(2*(abs(x(1)))));
+    %if (accel < 0)
+    %    accel = max(-5, accel);
+    %else
+    %    accel = min(5,accel);
+    %end
+    dxdt(2,1) = accel;
+end
 end
 
 function dxdt = f1(t,x)
